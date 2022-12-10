c5890e7d6b6b8944d103bb8336dbf042f2208e67
khalid <khalidmamdou7@gmail.com>
2022-12-10 12:39:18 +0200

    Req-4: Feat: Enabled Blending And Configured its Options

--- source/common/material/pipeline-state.hpp
+++ source/common/material/pipeline-state.hpp
@@ -66,6 +66,15 @@ namespace our {
             // regardless of the drawing operation attempted. 
             glColorMask(colorMask.r, colorMask.g, colorMask.b, colorMask.a);
 
+            if (blending.enabled) glEnable(GL_BLEND); else glDisable(GL_BLEND);
+            // Configure blending options
+            // Configure blending equation (GL_FUNC_ADD, GL_FUNC_SUBTRACT, GL_FUNC_REVERSE_SUBTRACT, GL_MIN, GL_MAX)
+            glBlendEquation(blending.equation);
+            // Configure blending function (GL_ZERO, GL_ONE, GL_SRC_COLOR, GL_ONE_MINUS_SRC_COLOR, GL_DST_COLOR, GL_ONE_MINUS_DST_COLOR, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_DST_ALPHA, GL_ONE_MINUS_DST_ALPHA, GL_CONSTANT_COLOR, GL_ONE_MINUS_CONSTANT_COLOR, GL_CONSTANT_ALPHA, GL_ONE_MINUS_CONSTANT_ALPHA, GL_SRC_ALPHA_SATURATE, GL_SRC1_COLOR, GL_ONE_MINUS_SRC1_COLOR, GL_SRC1_ALPHA, GL_ONE_MINUS_SRC1_ALPHA)
+            glBlendFunc(blending.sourceFactor, blending.destinationFactor);
+            // Configure blending constant color
+            glBlendColor(blending.constantColor.r, blending.constantColor.g, blending.constantColor.b, blending.constantColor.a);
+
         }
 
         // Given a json object, this function deserializes a PipelineState structure
