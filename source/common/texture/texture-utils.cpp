3acb910ebcfdf9b555532d40a89b41c42e082770
khalid <khalidmamdou7@gmail.com>
2022-12-17 14:06:07 +0200

    req-11: implement the empty fn in textures that create empty 2d texture

--- source/common/texture/texture-utils.cpp
+++ source/common/texture/texture-utils.cpp
@@ -9,7 +9,19 @@ our::Texture2D* our::texture_utils::empty(GLenum format, glm::ivec2 size){
     our::Texture2D* texture = new our::Texture2D();
     //TODO: (Req 11) Finish this function to create an empty texture with the given size and format
 
-    
+    texture->bind();
+    // glTexImage2D creates a texture object and allocates memory for it. It also sets the texture parameters
+    // it takes 9 arguments
+    // 1. target: the target texture, in this case GL_TEXTURE_2D, could be GL_TEXTURE_1D, GL_TEXTURE_3D, GL_TEXTURE_CUBE_MAP
+    // 2. level: the level of detail, 0 is the base image level, n is the nth mipmap reduction image
+    // 3. internalformat: the number of color components in the texture, could be GL_RED, GL_RG, GL_RGB, GL_RGBA, GL_DEPTH_COMPONENT, GL_DEPTH_STENCIL
+    // 4. width: the width of the texture image
+    // 5. height: the height of the texture image
+    // 6. border: the width of the border, must be 0
+    // 7. format: the format of the pixel data, could be GL_RED, GL_RG, GL_RGB, GL_RGBA, GL_DEPTH_COMPONENT, GL_DEPTH_STENCIL
+    // 8. type: the data type of the pixel data, could be GL_UNSIGNED_BYTE, GL_UNSIGNED_SHORT, GL_UNSIGNED_INT, GL_FLOAT
+    // 9. data: a pointer to the image data in memory, or 0 if no data is to be copied
+    glTexImage2D(GL_TEXTURE_2D, 0, format, size.x, size.y, 0, format, GL_UNSIGNED_BYTE, nullptr);
 
 
     return texture;
