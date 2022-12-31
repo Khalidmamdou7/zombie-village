e1b1fe7a43b04332fa639f0582c8891886e7fd3e
Seif Albaghdady <85033207+seifAlbaghdady@users.noreply.github.com>
2022-12-31 14:28:16 +0200

    trying to add on collision

--- source/common/systems/Collision.hpp
+++ source/common/systems/Collision.hpp
@@ -32,6 +32,15 @@ namespace our
         void update(World* world, float deltaTime) {
             vector<IsCollide*> allColl;
 
+            
+            for(auto entity : world->getEntities()){
+                auto collider =entity->getComponent<IsCollide>();
+                if(collider)
+                {
+                    allColl.push_back(collider);
+                }
+            }
+
             for(auto entity : world->getEntities()){
                 //cout<<"ana ana ana"<<endl;
                 auto collider =entity->getComponent<IsCollide>();
